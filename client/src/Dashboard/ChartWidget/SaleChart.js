33a9783301111b64ed6a16b6783c242bb0d14313
Prz-mek <kwasniewski.pj@gmail.com>
2022-01-28 11:40:02 +0100

    Clean up

--- client/src/Dashboard/ChartWidget/SaleChart.js
+++ client/src/Dashboard/ChartWidget/SaleChart.js
@@ -37,11 +37,9 @@ export default function SaleChart(props) {
     }
   };
 
-  console.log(data.labels.map((s) => t(s)));
-
   return props.type === "bar" ? (
-    <Bar data={{ ...data,/* labels: data.labels.map((s) => t(s)),*/ datasets: data.datasets.map((d) => {return {...d, label: t(d.label)};}) }} options={options} />
+    <Bar data={{ ...data, datasets: data.datasets.map((d) => {return {...d, label: t(d.label)};}) }} options={options} />
   ) : (
-    <Line data={{ ...data,/* labels: data.labels.map((s) => t(s)),*/ datasets: data.datasets.map((d) => {return {...d, label: t(d.label)};}) }} options={options} />
+    <Line data={{ ...data, datasets: data.datasets.map((d) => {return {...d, label: t(d.label)};}) }} options={options} />
   );
 }
